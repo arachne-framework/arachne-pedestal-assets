9130ce773b821aa7262a98f5da7c87ebdc267e16
Luke VanderHart <luke@vanderhart.net>
2017-05-09 16:35:53 -0400

    update to clojure 1.9-alpha16

--- src/arachne/pedestal_assets/dsl.clj
+++ src/arachne/pedestal_assets/dsl.clj
@@ -3,7 +3,7 @@
            [arachne.core.util :as util]
            [arachne.core.config.script :as script :refer [defdsl]]
            [arachne.core.config.specs :as ccs]
-           [clojure.spec :as s]
+           [clojure.spec.alpha :as s]
            [arachne.error :as e :refer [deferror error]]
            [clojure.string :as str]
            [arachne.http.dsl :as http-dsl]
